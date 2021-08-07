c123938abcb044a5a45860a3eb73a624a98deb1e
uraya <uraya@tcmobile.jp>
2021-08-07 10:03:46 +0900

    作成

---
+++ src/main/java/net/usysdev/githubactions/GithubActionTest.java
@@ -0,0 +1,15 @@
+/**
+ * Githubアクションのテスト
+ */
+package net.usysdev.githubactions;
+
+/**
+ * Githubアクションのテスト
+ */
+public class GithubActionTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello world");
+	}
+}
